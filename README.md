eecba72c2a9df6aca8a0fd462cf519ce6db81af6
wangquanjava <wangquanjava@163.com>
2016-10-28 17:27:41 +0800

    加入swagger

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# g05_ssm_template
-1. 修改pom.xml中artifactId、tomcat虚拟目录
-2. 修改web.xml中<display-name>、<servlet-name>
-3. jdbc.properties中数据库用户信息
-4. 如果不需要httpclent，删除com.git.evictor、applicationContext-httpclient.xml、com.git.domain.HttpResult、com.git.service.impl.ApiService即可
-5. 配置了一个默认的拦截器，没做任何处理，允许通过了com.git.intercepor.DemoInteceptor，springmvc.xml
-6. c3p0连接池
-7. 添加listener，用于监测服务器启动和关闭，web.xml\com.git.listener.ContextListener
+# g17_swagger_springmvc
+1. 使用swagger来完成动态抽出接口，并在线测试
--- src/main/java/com/git/controller/ControllerDemo.java
+++ src/main/java/com/git/controller/ControllerDemo.java
@@ -1,5 +1,19 @@
 package com.git.controller;
 
-public class ControllerDemo {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.wordnik.swagger.annotations.ApiOperation;
+
+@Controller
+@RequestMapping("/demoController")
+public class ControllerDemo {
+	@ResponseBody
+	@ApiOperation(value = "query api basic information",notes="this is notes")
+	@RequestMapping("get")
+	public String get(@RequestParam("id") String id){
+		return "success"+id;
+	}
 }
